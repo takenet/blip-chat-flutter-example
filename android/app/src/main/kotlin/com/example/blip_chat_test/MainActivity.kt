385b2dfa737320051a01536ba931ea81c8db5e25
Luis Rondow <luiseduardorondow@gmail.com>
2020-08-05 12:08:16 -0300

    Add the app

---
+++ android/app/src/main/kotlin/com/example/blip_chat_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blip_chat_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+const kAndroidUserAgent =
+    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
+
+const blipChatFlutterExampleTitle = 'BLiP Chat Flutter Example';
+
+String blipChatUrl =
+    'YOUR-BLIP-CHAT-URL';
+
+// ignore: prefer_collection_literals
+final Set<JavascriptChannel> jsChannels = [
+  JavascriptChannel(
+      name: 'Print',
+      onMessageReceived: (JavascriptMessage message) {
+        print(message.message);
+      }),
+].toSet();
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BLiP Chat Flutter Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (_) => const MyHomePage(title: blipChatFlutterExampleTitle),
+        '/chat': (_) {
+          return WebviewScaffold(
+            url: blipChatUrl,
+            javascriptChannels: jsChannels,
+            mediaPlaybackRequiresUserGesture: false,
+            appBar: AppBar(
+              title: const Text('BLiP Chat'),
+            ),
+            withZoom: true,
+            withLocalStorage: true,
+            hidden: true,
+            initialChild: Container(
+              color: Colors.redAccent,
+              child: const Center(
+                child: Text('Waiting.....'),
+              ),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios),
+                    onPressed: () {
+                      flutterWebViewPlugin.goBack();
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                      flutterWebViewPlugin.goForward();
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.autorenew),
+                    onPressed: () {
+                      flutterWebViewPlugin.reload();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Instance of WebView plugin
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
+  final _urlCtrl = TextEditingController(text: blipChatFlutterExampleTitle);
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterWebViewPlugin.close();
+
+    _urlCtrl.addListener(() {
+      blipChatUrl = _urlCtrl.text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text(blipChatFlutterExampleTitle),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(controller: _urlCtrl),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/chat');
+              },
+              child: const Text('Open chat'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
